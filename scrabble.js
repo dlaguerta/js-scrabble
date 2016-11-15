512d3ce53691ac430f763ae4b2c945c7db245865
Dianne Laguerta <diannelaguerta@gmail.com>
2016-11-15 14:24:58 -0800

    added title score constant to the Scrabble function

--- scrabble.js
+++ scrabble.js
@@ -1,8 +1,24 @@
-var Scrabble = function() {};
+var Scrabble = function() {
+  this.TILE_SCORES = {
+  1: ["A", "E", "I","O","U", "L", "N", "R", "S", "T"],
+  2: ["D", "G"],
+  3: ["B", "C", "M", "P"],
+  4: ["F", "H", "V", "W", "Y"],
+  5: ["K"],
+  8: ["J", "X"],
+  10: ["Q", "Z"]
+  };
+};
 
-// YOUR CODE HERE
+Scrabble.prototype.score = function(word) {
+  
+};
+// YOUR CODE ABOVE HERE
 Scrabble.prototype.helloWorld = function() {
   return 'hello world!';
 };
 
 module.exports = Scrabble;
+
+
+// to make something uppercase: .toUpperCase()
